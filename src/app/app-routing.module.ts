4bb39d941b5d7a068e970c20d08c782e84005a4e
p3cunha <pcls_@hotmail.com>
2020-11-30 17:42:57 -0300

    navBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TimerComponent } from './timer/timer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavBarComponent, children: [
+    { path: 'timer', component: TimerComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
